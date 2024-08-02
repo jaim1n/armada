cde126daa82cfe87bde2a7475f9f80f0fe447667
jaim1n <jaiminlayne@icloud.com>
2024-08-02 18:18:20 -0400

    Create generic.h

---
+++ common/user/generic.h
@@ -0,0 +1,12 @@
+#ifndef GENERIC_H
+#define GENERIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "sysent.h"
+#include "sys/mman.h"
+#include <assert.h>
+
+#endif // GENERIC_H
